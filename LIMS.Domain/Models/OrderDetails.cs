8a88de8ecdf57007f9f629a5e1418d7e6c091925
Shashank Saini <shashanksaini203@gmail.com>
2024-07-26 00:25:48 +0530

    OrderDetails Model added

---
+++ LIMS.Domain/Models/OrderDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace LIMS.Domain.Models
+{
+    public class OrderDetails
+    {
+        public int OrderId { get; set; }
+        public int InstrumentId { get; set; }
+        public Instrument Instrument { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string OrderedBy { get; set; }
+        public string AdditionalNotes { get; set; }
+    }
+}
