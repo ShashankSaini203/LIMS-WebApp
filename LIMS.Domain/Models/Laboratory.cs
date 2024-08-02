a17cd57fc28593a2f950a5311e75fb46768c3a8f
Shashank Saini <shashanksaini203@gmail.com>
2024-08-03 01:38:45 +0530

    Lab model added

---
+++ LIMS.Domain/Models/Laboratory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMS.Domain.Models
+{
+    public class Laboratory
+    {
+        public int LaboratoryId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ContactNumber { get; set; }
+        public List<Instrument> Instruments { get; set; }
+    }
+}
