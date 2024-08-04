11da3b9d7316e2a62b6c97901ed7c953c2c54ec8
Shashank Saini <shashanksaini203@gmail.com>
2024-08-05 01:33:56 +0530

    Models update

---
+++ LIMS.Domain/Models/CalibrationRecord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMS.Domain.Models
+{
+    public class CalibrationRecord
+    {
+        public int CalibrationRecordId { get; set; }
+        public int InstrumentId { get; set; }
+        public Instrument Instrument { get; set; }
+        public int TechnicianId { get; set; }
+        public Technician Technician { get; set; }
+        public DateTime CalibrationDate { get; set; }
+        public string Result { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ LIMS.Domain/Models/Technician.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMS.Domain.Models
+{
+    public class Technician
+    {
+        public int TechnicianId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<CalibrationRecord> CalibrationRecords { get; set; }
+    }
+}
