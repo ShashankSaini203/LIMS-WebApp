e6f1838d392f2db447822e18b7cb545c15ec9c55
Shashank Saini <shashanksaini203@gmail.com>
2024-08-06 20:57:07 +0530

    Repository added

---
+++ LIMS.Infrastructure/Repository/Commands/Laboratory/LabCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMS.Infrastructure.Repository.Commands.Laboratory
+{
+    internal class LabCommandRepository
+    {
+    }
+}
