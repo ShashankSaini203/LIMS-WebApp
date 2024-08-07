4508f80d196085dd5c8ac947ce0cd894cb8d09b3
Shashank Saini <shashanksaini203@gmail.com>
2024-08-07 21:05:56 +0530

    Generic base interface added

--- LIMS.Domain/Interfaces/Repository/Commands/BaseCommand/IBaseCommandRepository.cs
+++ LIMS.Domain/Interfaces/Repository/Commands/BaseCommand/IBaseCommandRepository.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LIMS.Domain.Interfaces.Repository.Commands.BaseCommand
+﻿namespace LIMS.Domain.Interfaces.Repository.Commands.BaseCommand
 {
     public interface IBaseCommandRepository<T>
     {
+        Task<T> CreateLaboratoryAsync(T entity);
+
+        Task<T> UpdateLaboratoryAsync(T entity);
+
+        Task<T> DeleteLaboratoryAsync(int entityId);
     }
 }
--- LIMS.Domain/Interfaces/Repository/Commands/ILabCommandRepository.cs
+++ LIMS.Domain/Interfaces/Repository/Commands/ILabCommandRepository.cs
@@ -1,15 +1,9 @@
 ﻿using LIMS.Domain.Interfaces.Repository.Commands.BaseCommand;
 using LIMS.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LIMS.Domain.Interfaces.Repository.Commands
 {
     public interface ILabCommandRepository : IBaseCommandRepository<Laboratory>
     {
-        Laboratory CreateLaboratory(Laboratory laboratory);
     }
 }
