2b8f80d1538159cd2ff465a0d562858bbfefe19c
Shashank Saini <shashanksaini203@gmail.com>
2024-08-08 21:35:57 +0530

    DataContext passed to ctor

--- LIMS.Infrastructure/Repository/Commands/LabCommandRepository.cs
+++ LIMS.Infrastructure/Repository/Commands/LabCommandRepository.cs
@@ -1,11 +1,17 @@
 ﻿using LIMS.Domain.Interfaces.Repository.Commands;
 using LIMS.Domain.Models;
+using LIMS.Infrastructure.Database.DBcontext;
 using LIMS.Infrastructure.Repository.Commands.BaseCommand;
 
 namespace LIMS.Infrastructure.Repository.Commands
 {
     public class LabCommandRepository : BaseCommandRepository<Laboratory>, ILabCommandRepository
     {
+        private readonly DataContext _dataContext;
+        public LabCommandRepository(DataContext dataContext) : base(dataContext)
+        {
+            _dataContext = dataContext;
+        }
         public override async Task<Laboratory> UpdateLaboratoryAsync(Laboratory entity)
         {
             throw new NotImplementedException();
