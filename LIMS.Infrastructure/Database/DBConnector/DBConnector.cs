8f1a00a0c2bf17a64a731f88230375c3290757b2
Shashank Saini <shashanksaini203@gmail.com>
2024-08-11 21:53:10 +0530

    DBconnector added

---
+++ LIMS.Infrastructure/Database/DBConnector/DBConnector.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+
+namespace LIMS.Infrastructure.Database.DBConnector
+{
+    internal class DBConnector : IDBConnector
+    {
+        private readonly IConfiguration _configuration;
+        public DBConnector(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            return new SqliteConnection(connectionString);
+        }
+    }
+}
---
+++ LIMS.Infrastructure/Database/DBConnector/IDBConnector.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace LIMS.Infrastructure.Database.DBConnector
+{
+    internal interface IDBConnector
+    {
+        IDbConnection CreateConnection();
+    }
+}
