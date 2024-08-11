29f3865a0568fd1009d5657e8be36db2715e02da
Shashank Saini <shashanksaini203@gmail.com>
2024-08-11 21:15:20 +0530

    Overridden Update Async

--- LIMS.Infrastructure/Repository/Commands/LabCommandRepository.cs
+++ LIMS.Infrastructure/Repository/Commands/LabCommandRepository.cs
@@ -2,6 +2,7 @@
 using LIMS.Domain.Models;
 using LIMS.Infrastructure.Database.DBcontext;
 using LIMS.Infrastructure.Repository.Commands.BaseCommand;
+using Microsoft.EntityFrameworkCore;
 
 namespace LIMS.Infrastructure.Repository.Commands
 {
@@ -12,9 +13,38 @@ namespace LIMS.Infrastructure.Repository.Commands
         {
             _dataContext = dataContext;
         }
-        public override async Task<Laboratory> UpdateLaboratoryAsync(Laboratory entity)
+        public override async Task<Laboratory> UpdateAsync(Laboratory entity)
         {
-            throw new NotImplementedException();
+            if (entity is null)
+            {
+                throw new ArgumentNullException("No Laboratory data provided");
+            }
+            try
+            {
+                var existingLab = await _dataContext.Set<Laboratory>().FindAsync(entity.Id);
+                if (existingLab is null)
+                {
+                    throw new KeyNotFoundException("No matching Laboratory found");
+                }
+
+                var newLab = new Laboratory()
+                {
+                    Id = existingLab.Id,
+                    Name = entity.Name ?? existingLab.Name,
+                    Location = entity.Location ?? existingLab.Location,
+                    ContactNumber = entity.ContactNumber ?? existingLab.ContactNumber,
+                    Instruments = entity.Instruments ?? existingLab.Instruments
+                };
+
+                var updatedLab = _dataContext.Set<Laboratory>().Update(newLab);
+                _dataContext.Entry(existingLab).State = EntityState.Modified;
+                await _dataContext.SaveChangesAsync();
+                return updatedLab.Entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
         }
     }
 }
