a6f5d2f7060beca5a3bdfab4cd323470750b5ca3
Shashank Saini <shashanksaini203@gmail.com>
2024-08-11 22:19:53 +0530

    Base Query Interface added

---
+++ LIMS.Domain/Interfaces/Repository/Query/BaseQuery/IBaseQueryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMS.Domain.Interfaces.Repository.Query.BaseQuery
+{
+    public interface IBaseQueryRepository<T>
+    {
+        IEnumerable<T> GetAllAsync();
+
+        T GetAsync(int id);
+    }
+}
