7fc901966afa4ae682b864d7f9aa3d7faf613eef
Shashank Saini <shashanksaini203@gmail.com>
2024-08-12 22:34:54 +0530

    Lab Query repo added

---
+++ LIMS.Infrastructure/Repository/Queries/LabQueryRepository.cs
@@ -0,0 +1,21 @@
+﻿using LIMS.Domain.Interfaces.Repository.Query;
+using LIMS.Domain.Models;
+using LIMS.Infrastructure.Database.DBConnector;
+using LIMS.Infrastructure.Repository.Queries.BaseQuery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMS.Infrastructure.Repository.Queries
+{
+    public class LabQueryRepository : BaseQueryRepository<Laboratory>, ILabQueryRepository
+    {
+        private DBConnector _dbConnector;
+        public LabQueryRepository(DBConnector dbConnector) : base(dbConnector)
+        {
+            _dbConnector = dbConnector;
+        }
+    }
+}
