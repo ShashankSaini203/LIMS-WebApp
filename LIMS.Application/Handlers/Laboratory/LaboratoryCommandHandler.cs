87e59427569d0085cde02989c27b08f3e39172b2
Shashank Saini <shashanksaini203@gmail.com>
2024-08-15 00:55:30 +0530

    Common Handler

--- LIMS.Application/Handlers/Laboratory/LaboratoryCommandHandler.cs
+++ LIMS.Application/Handlers/Laboratory/LaboratoryCommandHandler.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LIMS.Application.Handlers.Laboratory
 {
-    internal class CreateLaboratoryCommandHandler
+    internal class LaboratoryCommandHandler
     {
     }
 }
