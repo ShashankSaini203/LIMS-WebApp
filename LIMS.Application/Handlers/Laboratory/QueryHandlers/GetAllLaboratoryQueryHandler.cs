4957d84a0c06d1806ebeaff7a7680dec0a7ddd8e
Shashank Saini <shashanksaini203@gmail.com>
2024-08-16 01:40:07 +0530

    Query Handlers refactor

---
+++ LIMS.Application/Handlers/Laboratory/QueryHandlers/GetAllLaboratoryQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using LIMS.Application.Commands.Laboratory.Query;
+using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Handlers.Laboratory.QueryHandlers
+{
+    public class GetAllLaboratoryQueryHandler : IRequestHandler<GetAllLaboratoryQuery, LaboratoryResponse>
+    {
+        public Task<LaboratoryResponse> Handle(GetAllLaboratoryQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LIMS.Application/Handlers/Laboratory/QueryHandlers/GetLaboratoryByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using LIMS.Application.Queries.Laboratory;
+using LIMS.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMS.Application.Handlers.Laboratory.QueryHandlers
+{
+    public class GetLaboratoryByIdQueryHandler : IRequestHandler<GetLaboratoryByIdQuery, LaboratoryResponse>
+    {
+        public Task<LaboratoryResponse> Handle(GetLaboratoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
