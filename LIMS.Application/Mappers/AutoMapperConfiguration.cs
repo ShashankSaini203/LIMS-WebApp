957be2b1bdd0244cab6465d54b831b07939f4c67
Shashank Saini <shashanksaini203@gmail.com>
2024-08-17 23:32:03 +0530

    AutoMapper config added

---
+++ LIMS.Application/Mappers/AutoMapperConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace LIMS.Application.Mappers
+{
+    public static class AutoMapperConfiguration
+    {
+        private static IMapper CreateAutoMapper()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            }).CreateMapper();
+        }
+
+        public static IMapper Mapper => CreateAutoMapper();
+    }
+}
---
+++ LIMS.Application/Mappers/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using LIMS.Application.Commands.Laboratory;
+using LIMS.Application.Queries.Laboratory;
+using LIMS.Application.Responses;
+using LIMS.Domain.Models;
+
+namespace LIMS.Application.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Commands and Queries to Domain Contracts mapping
+            CreateMap<CreateLaboratoryCommand, Laboratory>().ReverseMap();
+            CreateMap<DeleteLaboratoryCommand, Laboratory>().ReverseMap();
+            CreateMap<UpdateLaboratoryCommand, Laboratory>().ReverseMap();
+            CreateMap<GetAllLaboratoryQuery, Laboratory>().ReverseMap();
+            CreateMap<GetLaboratoryByIdQuery, Laboratory>().ReverseMap();
+
+            //Responses to Domain Contracts mapping
+            CreateMap<LaboratoryResponse, Laboratory>().ReverseMap();
+            CreateMap<TechnicianResponse, Technician>().ReverseMap();
+            CreateMap<InstrumentResponse, Instrument>().ReverseMap();
+            CreateMap< CalibrationRecordResponse, CalibrationRecord>().ReverseMap();
+        }
+    }
+}
