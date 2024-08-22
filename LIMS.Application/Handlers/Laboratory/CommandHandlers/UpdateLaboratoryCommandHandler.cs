b66cb5afdc5a08a687bdb6d4f194b8c8498500e3
Shashank Saini <shashanksaini203@gmail.com>
2024-08-23 00:58:59 +0530

    Update Handler implemented

--- LIMS.Application/Handlers/Laboratory/CommandHandlers/UpdateLaboratoryCommandHandler.cs
+++ LIMS.Application/Handlers/Laboratory/CommandHandlers/UpdateLaboratoryCommandHandler.cs
@@ -1,14 +1,24 @@
 ﻿using LIMS.Application.Commands.Laboratory;
+using LIMS.Application.Mappers;
 using LIMS.Application.Responses;
+using LIMS.Domain.Interfaces.Repository.Commands;
 using MediatR;
 
 namespace LIMS.Application.Handlers.Laboratory.CommandHandlers
 {
     public class UpdateLaboratoryCommandHandler : IRequestHandler<UpdateLaboratoryCommand, LaboratoryResponse>
     {
-        public Task<LaboratoryResponse> Handle(UpdateLaboratoryCommand request, CancellationToken cancellationToken)
+        private ILabCommandRepository _labCommandRepository;
+
+        public async Task<LaboratoryResponse> Handle(UpdateLaboratoryCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var updatedLaboratoryEntity = AutoMapperConfiguration.Mapper.Map<Domain.Models.Laboratory>(request);
+
+            var result = await _labCommandRepository.UpdateAsync(updatedLaboratoryEntity);
+
+            var mappedResult = AutoMapperConfiguration.Mapper.Map<LaboratoryResponse>(result);
+
+            return mappedResult;
         }
     }
 }
