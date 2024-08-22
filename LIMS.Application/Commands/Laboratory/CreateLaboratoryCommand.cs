4e33ce30351a90653b02f6053f25b471d84032e7
Shashank Saini <shashanksaini203@gmail.com>
2024-08-22 23:50:12 +0530

    Create Lab page updated

--- LIMS.Application/Commands/Laboratory/CreateLaboratoryCommand.cs
+++ LIMS.Application/Commands/Laboratory/CreateLaboratoryCommand.cs
@@ -1,13 +1,20 @@
 ﻿using MediatR;
 using LIMS.Application.Responses;
+using System.ComponentModel.DataAnnotations;
 
 namespace LIMS.Application.Commands.Laboratory
 {
     public class CreateLaboratoryCommand : IRequest<LaboratoryResponse>
     {
         public string Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Location { get; set; }
+
+        [Required]
         public string ContactNumber { get; set; }
         public IEnumerable<InstrumentResponse> Instruments { get; set; }
     }
