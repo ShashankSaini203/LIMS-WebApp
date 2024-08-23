c564be804b47a3ed592790a761cab85e02da3c91
Shashank Saini <shashanksaini203@gmail.com>
2024-08-24 01:20:45 +0530

    Delete Handler Implemented

--- LIMS.Application/Handlers/Laboratory/CommandHandlers/DeleteLaboratoryCommandHandler.cs
+++ LIMS.Application/Handlers/Laboratory/CommandHandlers/DeleteLaboratoryCommandHandler.cs
@@ -1,11 +1,12 @@
 ﻿using LIMS.Application.Commands.Laboratory;
+using LIMS.Application.Mappers;
 using LIMS.Application.Responses;
 using LIMS.Domain.Interfaces.Repository.Commands;
 using MediatR;
 
 namespace LIMS.Application.Handlers.Laboratory.CommandHandlers
 {
-    public class DeleteLaboratoryCommandHandler : IRequestHandler<DeleteLaboratoryCommand, LaboratoryResponse>
+    public class DeleteLaboratoryCommandHandler : IRequestHandler<DeleteLaboratoryCommand, Unit>
     {
         private ILabCommandRepository _labCommandRepository;
 
@@ -14,9 +15,16 @@ namespace LIMS.Application.Handlers.Laboratory.CommandHandlers
             _labCommandRepository = labCommandRepository;
         }
 
-        public async Task<LaboratoryResponse> Handle(DeleteLaboratoryCommand request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(DeleteLaboratoryCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var entityToDelete = AutoMapperConfiguration.Mapper.Map<Domain.Models.Laboratory>(request);
+
+            if (entityToDelete != null)
+            {
+                await _labCommandRepository.DeleteAsync(entityToDelete);
+            }
+
+            return Unit.Value;
         }
     }
 }
