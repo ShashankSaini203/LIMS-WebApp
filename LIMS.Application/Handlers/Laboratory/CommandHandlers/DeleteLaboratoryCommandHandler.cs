8bd898528e87b347f4bdc0309dbbc21f476470e9
Shashank Saini <shashanksaini203@gmail.com>
2024-08-24 01:01:38 +0530

    Lab command Repo injected

--- LIMS.Application/Handlers/Laboratory/CommandHandlers/DeleteLaboratoryCommandHandler.cs
+++ LIMS.Application/Handlers/Laboratory/CommandHandlers/DeleteLaboratoryCommandHandler.cs
@@ -1,12 +1,20 @@
 ﻿using LIMS.Application.Commands.Laboratory;
 using LIMS.Application.Responses;
+using LIMS.Domain.Interfaces.Repository.Commands;
 using MediatR;
 
 namespace LIMS.Application.Handlers.Laboratory.CommandHandlers
 {
     public class DeleteLaboratoryCommandHandler : IRequestHandler<DeleteLaboratoryCommand, LaboratoryResponse>
     {
-        public Task<LaboratoryResponse> Handle(DeleteLaboratoryCommand request, CancellationToken cancellationToken)
+        private ILabCommandRepository _labCommandRepository;
+
+        public DeleteLaboratoryCommandHandler(ILabCommandRepository labCommandRepository)
+        {
+            _labCommandRepository = labCommandRepository;
+        }
+
+        public async Task<LaboratoryResponse> Handle(DeleteLaboratoryCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
