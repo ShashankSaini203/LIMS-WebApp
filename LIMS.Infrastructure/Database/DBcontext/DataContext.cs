a7a096af5a1acd833c4045547dfadebb96720d35
Shashank Saini <shashanksaini203@gmail.com>
2024-09-06 22:29:16 +0530

    DB context model builder for FK relationship

--- LIMS.Infrastructure/Database/DBcontext/DataContext.cs
+++ LIMS.Infrastructure/Database/DBcontext/DataContext.cs
@@ -13,6 +13,18 @@ namespace LIMS.Infrastructure.Database.DBcontext
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {     
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Laboratory>()
+                .HasMany(l => l.Instruments)
+                .WithOne(i => i.Laboratory)
+                .HasForeignKey(i => i.LaboratoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Instrument> Instruments { get; set; }
         public DbSet<CalibrationRecord> CalibrationRecords { get; set; }
         public DbSet<Laboratory> Laboratories { get; set; }
