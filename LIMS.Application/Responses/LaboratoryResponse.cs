bfd88c38838d85ace2e756b1b99067468e5f5e14
Shashank Saini <shashanksaini203@gmail.com>
2024-09-06 22:29:29 +0530

    Response updated

--- LIMS.Application/Responses/LaboratoryResponse.cs
+++ LIMS.Application/Responses/LaboratoryResponse.cs
@@ -5,6 +5,6 @@
         public string Name { get; set; }
         public string Location { get; set; }
         public string ContactNumber { get; set; }
-        public IEnumerable<InstrumentResponse> Instruments { get; set; }
+        public ICollection<InstrumentResponse> Instruments { get; set; }
     }
 }
