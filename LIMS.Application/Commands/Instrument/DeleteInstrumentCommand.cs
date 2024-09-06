2c968af44dab101a73277392c070e337c828df46
Shashank Saini <shashanksaini203@gmail.com>
2024-09-06 22:12:57 +0530

    Delete command added

---
+++ LIMS.Application/Commands/Instrument/DeleteInstrumentCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace LIMS.Application.Commands.Instrument
+{
+    public class DeleteInstrumentCommand : IRequest<Unit>
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public DeleteInstrumentCommand(int Id)
+        {
+            this.Id = Id;
+        }
+    }
+}
