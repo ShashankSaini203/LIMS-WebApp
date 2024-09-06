aeebe577fd158075c569d4eccc4ae3591605bdc6
Shashank Saini <shashanksaini203@gmail.com>
2024-09-06 22:12:44 +0530

    Create command added

---
+++ LIMS.Application/Commands/Instrument/CreateInstrumentCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using LIMS.Application.Responses;
+using System.ComponentModel.DataAnnotations;
+
+namespace LIMS.Application.Commands.Instrument
+{
+    public class CreateInstrumentCommand : IRequest<InstrumentResponse>
+    {
+        [Required, MinLength(3, ErrorMessage = "Name should be atleast 3 characters long")]
+        public string Name { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "Manufacturer name should be atleast 3 characters long")]
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public CalibrationRecordResponse CalibrationRecord { get; set; }
+        public string AdditionalNotes { get; set; }
+
+    }
+}
