e2afa2e4c892f130079a3ffbd90ece446c37fdd5
Shashank Saini <shashanksaini203@gmail.com>
2024-09-08 23:46:56 +0530

    DB updated

---
+++ LIMS.Infrastructure/Migrations/20240908180909_TestingFKreferences.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LIMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestingFKreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Instruments_CalibrationRecords_CalibrationRecordId",
+                table: "Instruments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Instruments_CalibrationRecordId",
+                table: "Instruments");
+
+            migrationBuilder.DropColumn(
+                name: "CalibrationRecordId",
+                table: "Instruments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CalibrationRecordId",
+                table: "Instruments",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Instruments_CalibrationRecordId",
+                table: "Instruments",
+                column: "CalibrationRecordId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Instruments_CalibrationRecords_CalibrationRecordId",
+                table: "Instruments",
+                column: "CalibrationRecordId",
+                principalTable: "CalibrationRecords",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
