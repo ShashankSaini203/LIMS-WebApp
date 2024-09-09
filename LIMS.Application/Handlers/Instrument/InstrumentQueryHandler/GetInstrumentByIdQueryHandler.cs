4ccb8fa670aed3457169d570c87d74c83da3b54a
Shashank Saini <shashanksaini203@gmail.com>
2024-09-09 23:41:50 +0530

    Get Instruments Query Handlers added

---
+++ LIMS.Application/Handlers/Instrument/InstrumentQueryHandler/GetInstrumentByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using LIMS.Application.Mappers;
+using LIMS.Application.Queries.Instrument;
+using LIMS.Application.Responses;
+using LIMS.Domain.Constants;
+using LIMS.Domain.Interfaces.Repository.Query;
+using MediatR;
+
+namespace LIMS.Application.Handlers.Instrument.InstrumentQueryHandler
+{
+    public class GetInstrumentByIdQueryHandler : IRequestHandler<GetInstrumentByIdQuery, InstrumentResponse>
+    {
+        private IInstrumentQueryRepository _instrumentQueryRepository;
+
+        public GetInstrumentByIdQueryHandler(IInstrumentQueryRepository instrumentQueryRepository)
+        {
+            _instrumentQueryRepository = instrumentQueryRepository;
+        }
+
+        public async Task<InstrumentResponse> Handle(GetInstrumentByIdQuery request, CancellationToken cancellationToken)
+        {
+            var instrumentEntity = _instrumentQueryRepository.GetAsyncById(request.Id, DataTables.InstrumentTable);
+            if (instrumentEntity == null)
+            {
+                throw new Exception("No instrument found with given Id");
+            }
+
+            return AutoMapperConfiguration.Mapper.Map<InstrumentResponse>(instrumentEntity);
+        }
+    }
+}
