8cf60a95e1821aef369f0b618a5a38d7569d54c5
Shashank Saini <shashanksaini203@gmail.com>
2024-09-09 23:41:29 +0530

    Get Instruments Queries added

---
+++ LIMS.Application/Queries/Instrument/GetAllInstrumentsQuery.cs
@@ -0,0 +1,9 @@
+﻿using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Queries.Instrument
+{
+    public class GetAllInstrumentsQuery : IRequest<IEnumerable<InstrumentResponse>>
+    {
+    }
+}
---
+++ LIMS.Application/Queries/Instrument/GetInstrumentByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Queries.Instrument
+{
+    public class GetInstrumentByIdQuery : IRequest<InstrumentResponse>
+    {
+        public int Id { get; set; }
+
+        public GetInstrumentByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
