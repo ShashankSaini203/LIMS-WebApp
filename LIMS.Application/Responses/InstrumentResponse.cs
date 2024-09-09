0b9f9335e4cad2a06a312ee4396cc4ba22adaa3b
Shashank Saini <shashanksaini203@gmail.com>
2024-09-10 00:01:57 +0530

    Instrument Response updated

--- LIMS.Application/Responses/InstrumentResponse.cs
+++ LIMS.Application/Responses/InstrumentResponse.cs
@@ -7,7 +7,6 @@
         public string Model { get; set; }
         public DateTime PurchaseDate { get; set; }
         public DateTime ExpiryDate { get; set; }
-        public CalibrationRecordResponse CalibrationRecord { get; set; }
         public string AdditionalNotes { get; set; }
     }
 }
