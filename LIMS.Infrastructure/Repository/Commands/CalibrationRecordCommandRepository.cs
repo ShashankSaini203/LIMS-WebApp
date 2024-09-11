bf5bee25503d16ab97511b51faae60e89d43c2ea
Shashank Saini <shashanksaini203@gmail.com>
2024-09-11 23:58:22 +0530

    Rename repos

--- LIMS.Infrastructure/Repository/Commands/CalibrationRecordCommandRepository.cs
+++ LIMS.Infrastructure/Repository/Commands/CalibrationRecordCommandRepository.cs
@@ -1,4 +1,6 @@
 ﻿using LIMS.Domain.Interfaces.Repository.Commands;
+using LIMS.Domain.Models;
+using LIMS.Infrastructure.Repository.Commands.BaseCommand;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace LIMS.Infrastructure.Repository.Commands
 {
-    internal class CalibrationCommandRepository : ICalibrationCommandRepository
+    internal class CalibrationRecordCommandRepository : BaseCommandRepository<CalibrationRecord> ICalibrationRecordCommandRepository
     {
     }
 }
