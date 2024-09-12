1bf6ac2c1d3b160d3a9486199e6e217ee48b447f
Shashank Saini <shashanksaini203@gmail.com>
2024-09-13 00:47:50 +0530

    MappingProfile refactor

--- LIMS.Application/Mappers/MappingProfile.cs
+++ LIMS.Application/Mappers/MappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using LIMS.Application.Commands.Instrument;
 using LIMS.Application.Commands.Laboratory;
+using LIMS.Application.Handlers.Instrument.InstrumentCommandHandlers;
+using LIMS.Application.Queries.Instrument;
 using LIMS.Application.Queries.Laboratory;
 using LIMS.Application.Responses;
 using LIMS.Domain.Models;
@@ -11,23 +13,39 @@ namespace LIMS.Application.Mappers
     {
         public MappingProfile()
         {
-            //Commands and Queries to Domain Contracts mapping
+            LaboratoryMappers();
+            InstrumentMappers();
+
+            DomainModelAndResponseMapper();
+        }
+
+        private void DomainModelAndResponseMapper()
+        {
+            //Domain Model and Response reverse mapping
+            CreateMap<LaboratoryResponse, Laboratory>().ReverseMap();
+            CreateMap<TechnicianResponse, Technician>().ReverseMap();
+            CreateMap<InstrumentResponse, Instrument>().ReverseMap();
+            CreateMap<CalibrationRecordResponse, CalibrationRecord>().ReverseMap();
+        }
+
+        private void LaboratoryMappers()
+        {
+            //Laboratory Commands, Queries and Domain model reverse mapping
             CreateMap<CreateLaboratoryCommand, Laboratory>().ReverseMap();
             CreateMap<DeleteLaboratoryCommand, Laboratory>().ReverseMap();
             CreateMap<UpdateLaboratoryCommand, Laboratory>().ReverseMap();
             CreateMap<GetAllLaboratoryQuery, Laboratory>().ReverseMap();
             CreateMap<GetLaboratoryByIdQuery, Laboratory>().ReverseMap();
+        }
 
-            //Responses to Domain Contracts mapping
-            CreateMap<LaboratoryResponse, Laboratory>().ReverseMap();
-            CreateMap<TechnicianResponse, Technician>().ReverseMap();
-            CreateMap<InstrumentResponse, Instrument>().ReverseMap();
-            CreateMap< CalibrationRecordResponse, CalibrationRecord>().ReverseMap();
-
+        private void InstrumentMappers()
+        {
+            //Instrument Commands, Queries and Domain model reverse mapping
             CreateMap<CreateInstrumentCommand, Instrument>().ReverseMap();
             CreateMap<UpdateInstrumentCommand, Instrument>().ReverseMap();
-
-
+            CreateMap<DeleteInstrumentCommandHandler, Instrument>().ReverseMap();
+            CreateMap<GetAllInstrumentsQuery, Instrument>().ReverseMap();
+            CreateMap<GetInstrumentByIdQuery, Instrument>().ReverseMap();
         }
     }
 }
