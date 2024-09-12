0d6b32a1be7eedcef9bb1baeaa65bb6992e67367
Shashank Saini <shashanksaini203@gmail.com>
2024-09-13 00:48:12 +0530

    GetAllInstrumentsQueryHandler

--- LIMS.Application/Handlers/Instrument/InstrumentQueryHandler/GetAllInstrumentsQueryHandler.cs
+++ LIMS.Application/Handlers/Instrument/InstrumentQueryHandler/GetAllInstrumentsQueryHandler.cs
@@ -1,14 +1,25 @@
-﻿using LIMS.Application.Queries.Instrument;
+﻿using LIMS.Application.Mappers;
+using LIMS.Application.Queries.Instrument;
 using LIMS.Application.Responses;
+using LIMS.Domain.Constants;
+using LIMS.Domain.Interfaces.Repository.Query;
 using MediatR;
 
 namespace LIMS.Application.Handlers.Instrument.InstrumentQueryHandler
 {
     public class GetAllInstrumentsQueryHandler : IRequestHandler<GetAllInstrumentsQuery, IEnumerable<InstrumentResponse>>
     {
+        private IInstrumentQueryRepository _instrumentQueryRepository;
+
+        public GetAllInstrumentsQueryHandler(IInstrumentQueryRepository instrumentQueryRepository)
+        {
+            _instrumentQueryRepository = instrumentQueryRepository;
+        }
         public Task<IEnumerable<InstrumentResponse>> Handle(GetAllInstrumentsQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var allInstruments = _instrumentQueryRepository.GetAllAsync(DataTables.InstrumentTable);
+
+            return AutoMapperConfiguration.Mapper.Map<IEnumerable<InstrumentResponse>>(allInstruments);
         }
     }
 }
