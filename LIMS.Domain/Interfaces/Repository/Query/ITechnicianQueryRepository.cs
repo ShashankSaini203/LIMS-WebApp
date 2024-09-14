286accb053d20337a986de1e24bee841c7bd0398
Shashank Saini <shashanksaini203@gmail.com>
2024-09-14 21:15:46 +0530

    Interfaces added

---
+++ LIMS.Domain/Interfaces/Repository/Query/ITechnicianQueryRepository.cs
@@ -0,0 +1,9 @@
+﻿using LIMS.Domain.Interfaces.Repository.Query.BaseQuery;
+using LIMS.Domain.Models;
+
+namespace LIMS.Domain.Interfaces.Repository.Query
+{
+    public interface ITechnicianQueryRepository : IBaseQueryRepository<Technician>
+    {
+    }
+}
