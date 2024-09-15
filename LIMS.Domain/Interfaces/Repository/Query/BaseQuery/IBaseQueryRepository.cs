fa4d03c0f32a6c794e91914f18dc5aadc0446e9c
Shashank Saini <shashanksaini203@gmail.com>
2024-09-15 21:26:48 +0530

    Cleanup

--- LIMS.Domain/Interfaces/Repository/Query/BaseQuery/IBaseQueryRepository.cs
+++ LIMS.Domain/Interfaces/Repository/Query/BaseQuery/IBaseQueryRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LIMS.Domain.Interfaces.Repository.Query.BaseQuery
+﻿namespace LIMS.Domain.Interfaces.Repository.Query.BaseQuery
 {
     public interface IBaseQueryRepository<T>
     {
