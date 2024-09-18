73db65f7b21c65b8b1c000d01645f9510b0340b2
Shashank Saini <shashanksaini203@gmail.com>
2024-09-18 21:42:28 +0530

    Interfaces update

--- LIMS.Domain/Interfaces/Repository/Commands/IOrderDetailsCommandRepository.cs
+++ LIMS.Domain/Interfaces/Repository/Commands/IOrderDetailsCommandRepository.cs
@@ -3,7 +3,7 @@ using LIMS.Domain.Models;
 
 namespace LIMS.Domain.Interfaces.Repository.Commands
 {
-    public interface IOrderCommandRepository : IBaseCommandRepository<OrderDetails>
+    public interface IOrderDetailsCommandRepository : IBaseCommandRepository<OrderDetails>
     {
     }
 }
--- LIMS.Domain/Interfaces/Repository/Query/IOrderDetailsQueryRepository.cs
+++ LIMS.Domain/Interfaces/Repository/Query/IOrderDetailsQueryRepository.cs
@@ -3,7 +3,7 @@ using LIMS.Domain.Models;
 
 namespace LIMS.Domain.Interfaces.Repository.Query
 {
-    public interface IOrderQueryRepository : IBaseQueryRepository<OrderDetails>
+    public interface IOrderDetailsQueryRepository : IBaseQueryRepository<OrderDetails>
     {
     }
 }
