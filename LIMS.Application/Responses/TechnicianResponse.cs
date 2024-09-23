bf09dd6056ef9bc0062cb8b6053c6eecd8619455
Shashank Saini <shashanksaini203@gmail.com>
2024-09-23 23:59:13 +0530

    Response

--- LIMS.Application/Responses/TechnicianResponse.cs
+++ LIMS.Application/Responses/TechnicianResponse.cs
@@ -6,5 +6,7 @@
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public string Status { get; set; }
+
     }
 }
