21d01d93c5c671be40d5926c76b0ea271c6689cb
Shashank Saini <shashanksaini203@gmail.com>
2024-09-26 02:14:23 +0530

    Data Context Model Creation updates

--- LIMS.Infrastructure/Database/DBcontext/DataContext.cs
+++ LIMS.Infrastructure/Database/DBcontext/DataContext.cs
@@ -1,26 +1,45 @@
 ﻿using LIMS.Domain.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LIMS.Infrastructure.Database.DBcontext
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {     
+        {
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Laboratory>()
-                .HasMany(l => l.Instruments)
-                .WithOne(i => i.Laboratory)
-                .HasForeignKey(i => i.LaboratoryId)
-                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<Instrument>()
+                    .HasOne<Laboratory>()
+                    .WithMany()
+                    .HasForeignKey(i => i.LaboratoryId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CalibrationRecord>()
+                    .HasOne<Instrument>()
+                    .WithMany()
+                    .HasForeignKey(i => i.InstrumentId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CalibrationRecord>()
+                    .HasOne<Technician>()
+                    .WithMany()
+                    .HasForeignKey(i => i.TechnicianId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<OrderDetails>()
+                    .HasOne<Laboratory>()
+                    .WithMany()
+                    .HasForeignKey(i => i.LaboratoryId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Instrument>()
+                    .HasOne<Laboratory>()
+                    .WithMany()
+                    .HasForeignKey(i => i.LaboratoryId)
+                    .OnDelete(DeleteBehavior.Cascade);
 
             base.OnModelCreating(modelBuilder);
         }
