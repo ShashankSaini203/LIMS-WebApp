11a3510fbe4a088ee65adc0ea2da113ea8bd1a43
Shashank Saini <shashanksaini203@gmail.com>
2024-09-26 02:13:49 +0530

    Update Calibration Record model

--- LIMS.Domain/Models/CalibrationRecord.cs
+++ LIMS.Domain/Models/CalibrationRecord.cs
@@ -1,14 +1,16 @@
-﻿using LIMS.Domain.Models.Base;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace LIMS.Domain.Models
 {
-    public class CalibrationRecord : BaseModel
+    public class CalibrationRecord
     {
+        [Key]
+        public int CalibrationRecordId { get; set; }
         public DateTime CurrentCalibrationDate { get; set; }
         public DateTime NextCalibrationDueDate { get; set; }
         public string Result { get; set; }
         public string Notes { get; set; }
+        public int InstrumentId { get; set; }
         public int TechnicianId { get; set; }
-        public string TechnicianName { get; set; }
     }
 }
