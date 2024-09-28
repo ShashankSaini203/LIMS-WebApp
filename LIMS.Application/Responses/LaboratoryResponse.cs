b5b8d264accdd9456e268312515253e61d3f205a
Shashank Saini <shashanksaini203@gmail.com>
2024-09-29 03:05:03 +0530

    Fixing Responses and Views

--- LIMS.Application/Responses/LaboratoryResponse.cs
+++ LIMS.Application/Responses/LaboratoryResponse.cs
@@ -2,6 +2,7 @@
 {
     public class LaboratoryResponse
     {
+        public int LaboratoryId { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
         public string ContactNumber { get; set; }
