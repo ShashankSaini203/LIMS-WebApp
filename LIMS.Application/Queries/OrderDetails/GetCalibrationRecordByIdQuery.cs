1eff58226feed514b931b8faa51f572454b8ee56
Shashank Saini <shashanksaini203@gmail.com>
2024-10-04 22:32:51 +0530

    Get OrderDetails by Id added

---
+++ LIMS.Application/Queries/OrderDetails/GetCalibrationRecordByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Queries.OrderDetails
+{
+    public class GetOrderDetailsByIdQuery : IRequest<OrderDetailsResponse>
+    {
+        public int Id { get; set; }
+
+        public GetOrderDetailsByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
