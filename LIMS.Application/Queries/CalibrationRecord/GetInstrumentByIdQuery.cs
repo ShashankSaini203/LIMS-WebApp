e85b846fee17d3dfd53f86a79891b36c9767edec
Shashank Saini <shashanksaini203@gmail.com>
2024-10-04 22:31:52 +0530

    Get CalibrationRecord by Id added

---
+++ LIMS.Application/Queries/CalibrationRecord/GetInstrumentByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Queries.CalibrationRecord
+{
+    public class GetCalibrationRecordByIdQuery : IRequest<CalibrationRecordResponse>
+    {
+        public int Id { get; set; }
+
+        public GetCalibrationRecordByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
