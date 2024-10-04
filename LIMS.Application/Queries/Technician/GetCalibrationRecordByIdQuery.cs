ba6010d813b559f5dda7c282beadf95d69d0d1bc
Shashank Saini <shashanksaini203@gmail.com>
2024-10-04 22:33:28 +0530

    Technician by Id added

---
+++ LIMS.Application/Queries/Technician/GetCalibrationRecordByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Queries.Technician
+{
+    public class GetTechnicianByIdQuery : IRequest<TechnicianResponse>
+    {
+        public int Id { get; set; }
+
+        public GetTechnicianByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
