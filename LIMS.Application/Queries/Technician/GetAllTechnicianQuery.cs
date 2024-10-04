8618fbec05f92fd05e3121790d4e705feba1987e
Shashank Saini <shashanksaini203@gmail.com>
2024-10-04 22:30:47 +0530

    GetAllTechnicianQuery added

---
+++ LIMS.Application/Queries/Technician/GetAllTechnicianQuery.cs
@@ -0,0 +1,9 @@
+﻿using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Queries.Technician
+{
+    public class GetAllTechnicianQuery : IRequest<IEnumerable<TechnicianResponse>>
+    {
+    }
+}
