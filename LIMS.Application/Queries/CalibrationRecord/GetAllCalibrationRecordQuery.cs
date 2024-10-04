97dd6f12bd677d68db41650ccd785091ab5c25c4
Shashank Saini <shashanksaini203@gmail.com>
2024-10-04 22:29:00 +0530

    GetAllCalibrationRecordQuery added

---
+++ LIMS.Application/Queries/CalibrationRecord/GetAllCalibrationRecordQuery.cs
@@ -0,0 +1,9 @@
+﻿using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Queries.CalibrationRecord
+{
+    public class GetAllCalibrationRecordQuery : IRequest<IEnumerable<CalibrationRecordResponse>>
+    {
+    }
+}
