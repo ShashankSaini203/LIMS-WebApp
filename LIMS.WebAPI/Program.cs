ca270162c033539681e3923bd16bfc38b0b8bb94
Shashank Saini <shashanksaini203@gmail.com>
2024-10-15 20:11:37 +0530

    LocalDB added

--- LIMS.WebAPI/Program.cs
+++ LIMS.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using LIMS.Infrastructure.Database.DBcontext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    //options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDBConnection"));
+});
+
+
 
 var app = builder.Build();
 
