3125fe3ef1ceaba2a0ad771137349c4c35445c70
Shashank Saini <shashanksaini203@gmail.com>
2024-10-22 19:48:39 +0530

    Command updated

--- LIMS.Application/Commands/Laboratory/CreateLaboratoryCommand.cs
+++ LIMS.Application/Commands/Laboratory/CreateLaboratoryCommand.cs
@@ -6,14 +6,8 @@ namespace LIMS.Application.Commands.Laboratory
 {
     public class CreateLaboratoryCommand : IRequest<LaboratoryResponse>
     {
-        [Required, MinLength(3, ErrorMessage ="Name should be atleast 3 characters long")]
         public string Name { get; set; }
-
-        [Required, MinLength(2, ErrorMessage = "Location should be atleast 2 characters long")]
         public string Location { get; set; }
-
-        [Required, Phone(ErrorMessage = "Invalid Contact Number"), DataType(DataType.PhoneNumber)]
         public string ContactNumber { get; set; }
-        public ICollection<InstrumentResponse>? Instruments { get; set; }
     }
 }
