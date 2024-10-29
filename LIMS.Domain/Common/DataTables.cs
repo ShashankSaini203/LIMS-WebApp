cc6e323d2dadaaf57b6e7369075649417940f5d3
Shashank Saini <shashanksaini203@gmail.com>
2024-10-30 00:16:38 +0530

    Id column names added

--- LIMS.Domain/Common/DataTables.cs
+++ LIMS.Domain/Common/DataTables.cs
@@ -8,4 +8,13 @@
         public const string CalibrationRecordTable = "CALIBRATIONRECORDS";
         public const string OrderDetailTable = "ORDERDETAILS";
     }
+
+    public static class DataColumns
+    {
+        public const string LaboratoryId = "LABORATORYID";
+        public const string InstrumentId = "INSTRUMENTID";
+        public const string TechnicianId = "TECHNICIANID";
+        public const string CalibrationRecordId = "CALIBRATIONRECORDID";
+        public const string OrderId = "ORDERID";
+    }
 }
