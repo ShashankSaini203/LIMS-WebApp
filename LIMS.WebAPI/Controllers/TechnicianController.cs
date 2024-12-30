000a09c4089456fcb0e9370543d8feebe925a708
Shashank Saini <shashanksaini203@gmail.com>
2024-12-31 00:01:49 +0530

    Technician Controller Added

---
+++ LIMS.WebAPI/Controllers/TechnicianController.cs
@@ -0,0 +1,13 @@
+﻿using LIMS.Application.Queries.Technician;
+using LIMS.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LIMS.WebAPI.Controllers
+{
+    public class TechnicianController : BaseController
+    {
+
+
+    }
+}
