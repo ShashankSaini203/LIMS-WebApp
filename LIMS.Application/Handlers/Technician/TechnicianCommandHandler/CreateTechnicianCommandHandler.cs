6c50f96b63b2eee5c70da8ff940b43422d65f915
Shashank Saini <shashanksaini203@gmail.com>
2025-01-04 00:16:05 +0530

    Handle method implemented

--- LIMS.Application/Handlers/Technician/TechnicianCommandHandler/CreateTechnicianCommandHandler.cs
+++ LIMS.Application/Handlers/Technician/TechnicianCommandHandler/CreateTechnicianCommandHandler.cs
@@ -1,10 +1,17 @@
 ﻿using LIMS.Application.Commands.Technician;
 using LIMS.Application.Responses;
+using LIMS.Domain.Interfaces.Repository.Commands;
 using MediatR;
 
 namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
 {
     public class CreateTechnicianCommandHandler : IRequestHandler<CreateTechnicianCommand, TechnicianResponse>
     {
+
+
+        public async Task<TechnicianResponse> Handle(CreateTechnicianCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
