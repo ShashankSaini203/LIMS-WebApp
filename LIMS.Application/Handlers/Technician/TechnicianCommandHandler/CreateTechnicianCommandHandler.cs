7cdc60c4dcce07e17d11587e2bb8d987b51a9430
Shashank Saini <shashanksaini203@gmail.com>
2025-01-04 00:09:09 +0530

    Cleanups

--- LIMS.Application/Handlers/Technician/TechnicianCommandHandler/CreateTechnicianCommandHandler.cs
+++ LIMS.Application/Handlers/Technician/TechnicianCommandHandler/CreateTechnicianCommandHandler.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
+﻿namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
 {
     internal class CreateTechnicianCommandHandler
     {
