9b91045ac6680aa17e6cc35bf7542dc661bcd950
Shashank Saini <shashanksaini203@gmail.com>
2025-01-06 20:36:54 +0530

    Technician Command fix

--- LIMS.Application/Commands/Technician/CreateTechnicianCommand.cs
+++ LIMS.Application/Commands/Technician/CreateTechnicianCommand.cs
@@ -6,19 +6,14 @@ namespace LIMS.Application.Commands.Technician
 {
     public class CreateTechnicianCommand : IRequest<TechnicianResponse>
     {
-        [Required, MinLength(3, ErrorMessage = "Name should be atleast 3 characters long")]
-        public string Name { get; set; }
+        [Required, MinLength(3, ErrorMessage = "FirstName should be atleast 3 characters long")]
+        public string FirstName { get; set; }
 
-        [Required, MinLength(3, ErrorMessage = "Manufacturer name should be atleast 3 characters long")]
-        public string Manufacturer { get; set; }
-        public string Model { get; set; }
-
-        [Required]
-        public DateTime PurchaseDate { get; set; }
-        public DateTime ExpiryDate { get; set; }
-        public CalibrationRecordResponse? CalibrationRecord { get; set; }
-        public string AdditionalNotes { get; set; }
+        [Required, MinLength(3, ErrorMessage = "LastName should be atleast 3 characters long")]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Status { get; set; }
         public int LaboratoryId { get; set; }
-
     }
 }
