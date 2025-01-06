0d5eaa16dea1a24049bc30c6ad0d8c49244d0176
Shashank Saini <shashanksaini203@gmail.com>
2025-01-06 20:42:23 +0530

    Model fix

--- LIMS.Domain/Models/Technician.cs
+++ LIMS.Domain/Models/Technician.cs
@@ -16,5 +16,7 @@ namespace LIMS.Domain.Models
         public string Email { get; set; }
         public string Phone { get; set; }
         public Status Status { get; set; }
+        public int LaboratoryId { get; set; }
+
     }
 }
