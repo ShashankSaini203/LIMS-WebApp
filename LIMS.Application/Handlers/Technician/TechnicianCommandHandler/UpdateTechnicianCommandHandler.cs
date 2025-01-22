c6308b63696b9dee4fc7c41d8e539a076ca8c85b
Shashank Saini <shashanksaini203@gmail.com>
2025-01-22 21:32:07 +0530

    Command handler fix

--- LIMS.Application/Handlers/Technician/TechnicianCommandHandler/UpdateTechnicianCommandHandler.cs
+++ LIMS.Application/Handlers/Technician/TechnicianCommandHandler/UpdateTechnicianCommandHandler.cs
@@ -1,6 +1,16 @@
-﻿namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
+﻿using LIMS.Application.Commands.Technician;
+using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
 {
-    public class UpdateTechnicianCommandHandler
+    public class UpdateTechnicianCommandHandler : IRequestHandler<UpdateTechnicianCommand, TechnicianResponse>
     {
+        public UpdateTechnicianCommandHandler() { }
+
+        public async Task<TechnicianResponse> IRequestHandler<UpdateTechnicianCommand, TechnicianResponse>.Handle(UpdateTechnicianCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
