8bfd0d5643dd7356e807f81a5b5ebb39f9acb525
Shashank Saini <shashanksaini203@gmail.com>
2025-01-27 19:26:40 +0530

    Handle implemented

--- LIMS.Application/Handlers/Technician/TechnicianCommandHandler/UpdateTechnicianCommandHandler.cs
+++ LIMS.Application/Handlers/Technician/TechnicianCommandHandler/UpdateTechnicianCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using LIMS.Application.Commands.Technician;
+using LIMS.Application.Mappers;
 using LIMS.Application.Responses;
 using LIMS.Domain.Interfaces.Repository.Commands;
+using LIMS.Domain.Models;
 using MediatR;
 
 namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
@@ -16,7 +18,18 @@ namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
 
         public async Task<TechnicianResponse> Handle(UpdateTechnicianCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var UpdateTechnicianEntity = AutoMapperConfiguration.Mapper.Map<Domain.Models.Technician>(request);
+            if (UpdateTechnicianEntity == null)
+            {
+                throw new Exception("Error with the mapper");
+            }
+            var result = _technicianCommandRepository.UpdateAsync(UpdateTechnicianEntity);
+            var mappedResult = AutoMapperConfiguration.Mapper.Map<TechnicianResponse>(result);
+            if (mappedResult == null)
+            {
+                throw new Exception("Error with the mapper");
+            }
+            return mappedResult;
         }
     }
 }
