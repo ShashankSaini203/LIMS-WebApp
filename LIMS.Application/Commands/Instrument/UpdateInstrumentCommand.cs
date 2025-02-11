8ad64bb59f29d8c289a28e9fb1d1502cf7ded1e5
Shashank Saini <shashanksaini203@gmail.com>
2025-02-11 23:09:55 +0530

    UpdateInstrumentCommand updated

--- LIMS.Application/Commands/Instrument/UpdateInstrumentCommand.cs
+++ LIMS.Application/Commands/Instrument/UpdateInstrumentCommand.cs
@@ -7,15 +7,9 @@ namespace LIMS.Application.Commands.Instrument
     public class UpdateInstrumentCommand : IRequest<InstrumentResponse>
     {
         public string Id { get; set; }
-
-        [Required, MinLength(3, ErrorMessage = "Name should be atleast 3 characters long")]
         public string Name { get; set; }
-
-        [Required, MinLength(3, ErrorMessage = "Manufacturer name should be atleast 3 characters long")]
         public string Manufacturer { get; set; }
         public string Model { get; set; }
-
-        [Required]
         public DateTime PurchaseDate { get; set; }
         public DateTime ExpiryDate { get; set; }
         public string AdditionalNotes { get; set; }
