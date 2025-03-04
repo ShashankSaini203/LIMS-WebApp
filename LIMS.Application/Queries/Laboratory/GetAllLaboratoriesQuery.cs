49fd2a7fa0645852e89b0abed7300539fb323acb
Shashank Saini <shashanksaini203@gmail.com>
2025-03-04 20:13:37 +0530

    Get all Laboratories query naming fixes

---
+++ LIMS.Application/Queries/Laboratory/GetAllLaboratoriesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using LIMS.Application.Responses;
+
+namespace LIMS.Application.Queries.Laboratory
+{
+    public class GetAllLaboratoriesQuery : IRequest<IEnumerable<LaboratoryResponse>>
+    {
+    }
+}
