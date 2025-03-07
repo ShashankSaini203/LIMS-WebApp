3d92e793f1e3d777ba7c149aea625eb8c3a63f8c
Shashank Saini <shashanksaini203@gmail.com>
2025-03-07 20:18:18 +0530

    Technicians Query handler access level to public

--- LIMS.Application/Handlers/Technician/TechnicianQueryHandler/GetAllTechniciansQueryHandler.cs
+++ LIMS.Application/Handlers/Technician/TechnicianQueryHandler/GetAllTechniciansQueryHandler.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LIMS.Application.Handlers.Technician.TechnicianQueryHandler
 {
-    internal class GetAllTechniciansQueryHandler
+    public class GetAllTechniciansQueryHandler
     {
     }
 }
