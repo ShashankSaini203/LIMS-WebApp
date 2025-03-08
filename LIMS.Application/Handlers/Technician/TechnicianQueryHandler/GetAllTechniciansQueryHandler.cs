3a78132dfe7cfc8451f120f7b845a03667aaeda3
Shashank Saini <shashanksaini203@gmail.com>
2025-03-08 20:42:28 +0530

    Get All technicians Query Handler done

--- LIMS.Application/Handlers/Technician/TechnicianQueryHandler/GetAllTechniciansQueryHandler.cs
+++ LIMS.Application/Handlers/Technician/TechnicianQueryHandler/GetAllTechniciansQueryHandler.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LIMS.Application.Mappers;
+using LIMS.Application.Queries.Technician;
+using LIMS.Application.Responses;
+using LIMS.Domain.Common;
+using LIMS.Domain.Interfaces.Repository.Query;
+using MediatR;
 
 namespace LIMS.Application.Handlers.Technician.TechnicianQueryHandler
 {
-    public class GetAllTechniciansQueryHandler
+    public class GetAllTechniciansQueryHandler : IRequestHandler<GetAllTechniciansQuery, IEnumerable<TechnicianResponse>>
     {
+        private readonly ITechnicianQueryRepository _technicianQueryRepository;
+
+        public GetAllTechniciansQueryHandler(ITechnicianQueryRepository technicianQueryRepository)
+        {
+            _technicianQueryRepository = technicianQueryRepository;
+        }
+
+        public async Task<IEnumerable<TechnicianResponse>> Handle(GetAllTechniciansQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _technicianQueryRepository.GetAllAsync(DataTables.TechnicianTable);
+            var mappedResponse = AutoMapperConfiguration.Mapper.Map<IEnumerable<TechnicianResponse>>(result);
+            if (mappedResponse == null)
+            {
+                throw new ApplicationException("Unable to map due to an issue with mapper.");
+            }
+
+            return mappedResponse;
+        }
     }
 }
