54e4b69b787a1d9125c4f4df6007c4a16c70d615
Shashank Saini <shashanksaini203@gmail.com>
2025-03-10 18:28:18 +0530

    Handle method implemented

--- LIMS.Application/Handlers/Technician/TechnicianCommandHandler/DeleteTechnicianCommandHandler.cs
+++ LIMS.Application/Handlers/Technician/TechnicianCommandHandler/DeleteTechnicianCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using LIMS.Application.Commands.Technician;
+using LIMS.Application.Mappers;
 using LIMS.Domain.Interfaces.Repository.Commands;
 using MediatR;
 
@@ -15,6 +16,21 @@ namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
 
         public async Task<Unit> Handle(DeleteTechnicianCommand request, CancellationToken cancellationToken)
         {
+            try
+            {
+                var entityToDelete = await _technician
+                var technicianEntity = AutoMapperConfiguration.Mapper.Map<Domain.Models.Technician>(request.TechnicianId);
+                if (technicianEntity == null)
+                {
+                    throw new Exception
+                }
+                var response = await _technicianCommandRepository.DeleteAsync(technicianEntity.TechnicianId);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw new 
+            }
 
         }
     }
