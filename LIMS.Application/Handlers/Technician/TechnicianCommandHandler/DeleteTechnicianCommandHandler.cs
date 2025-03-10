55e056aab69b12350a4db586c0b5af75a22ca76a
Shashank Saini <shashanksaini203@gmail.com>
2025-03-10 18:22:49 +0530

    Delete Handler Updates

--- LIMS.Application/Handlers/Technician/TechnicianCommandHandler/DeleteTechnicianCommandHandler.cs
+++ LIMS.Application/Handlers/Technician/TechnicianCommandHandler/DeleteTechnicianCommandHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
+﻿using LIMS.Application.Commands.Technician;
+using LIMS.Domain.Interfaces.Repository.Commands;
+using MediatR;
+
+namespace LIMS.Application.Handlers.Technician.TechnicianCommandHandler
 {
-    public class DeleteTechnicianCommandHandler
+    public class DeleteTechnicianCommandHandler : IRequestHandler<DeleteTechnicianCommand, Unit>
     {
+        private readonly ITechnicianCommandRepository _technicianCommandRepository;
+
+        public DeleteTechnicianCommandHandler(ITechnicianCommandRepository technicianCommandRepository)
+        {
+            _technicianCommandRepository = technicianCommandRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteTechnicianCommand request, CancellationToken cancellationToken)
+        {
+
+        }
     }
 }
