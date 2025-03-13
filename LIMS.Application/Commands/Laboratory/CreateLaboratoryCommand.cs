8d2ed39f157e7e3091c7afa174de94b0b0ec4a1c
Shashank Saini <shashanksaini203@gmail.com>
2025-03-13 18:30:08 +0530

    CreateLaboratoryCommand updated

--- LIMS.Application/Commands/Laboratory/CreateLaboratoryCommand.cs
+++ LIMS.Application/Commands/Laboratory/CreateLaboratoryCommand.cs
@@ -8,5 +8,7 @@ namespace LIMS.Application.Commands.Laboratory
         public string Name { get; set; }
         public string Location { get; set; }
         public string ContactNumber { get; set; }
+
+        public string LaboratoryId { get; set; }
     }
 }
