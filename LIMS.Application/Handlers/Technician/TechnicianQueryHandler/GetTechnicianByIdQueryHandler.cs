b42349becf70946909c9b0058dd174013e843978
Shashank Saini <shashanksaini203@gmail.com>
2025-03-18 18:38:18 +0530

    TechnicianQueryRepository DI

--- LIMS.Application/Handlers/Technician/TechnicianQueryHandler/GetTechnicianByIdQueryHandler.cs
+++ LIMS.Application/Handlers/Technician/TechnicianQueryHandler/GetTechnicianByIdQueryHandler.cs
@@ -1,11 +1,19 @@
 ﻿using LIMS.Application.Queries.Technician;
 using LIMS.Application.Responses;
+using LIMS.Domain.Interfaces.Repository.Query;
 using MediatR;
 
 namespace LIMS.Application.Handlers.Technician.TechnicianQueryHandler
 {
     public class GetTechnicianByIdQueryHandler : IRequestHandler<GetTechnicianByIdQuery, TechnicianResponse>
     {
+        private readonly ITechnicianQueryRepository _technicianQueryRepository;
+
+        public GetTechnicianByIdQueryHandler(ITechnicianQueryRepository technicianQueryRepository)
+        {
+            _technicianQueryRepository = technicianQueryRepository;
+        }
+
         public Task<TechnicianResponse> Handle(GetTechnicianByIdQuery request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
