5d51162a9de67f543ce2330b35879b2333864508
Shashank Saini <shashanksaini203@gmail.com>
2025-03-27 17:48:35 +0530

    UpdateLab command ID renamed

--- LIMS.Application/Commands/Laboratory/UpdateLaboratoryCommand.cs
+++ LIMS.Application/Commands/Laboratory/UpdateLaboratoryCommand.cs
@@ -5,7 +5,7 @@ namespace LIMS.Application.Commands.Laboratory
 {
     public class UpdateLaboratoryCommand : IRequest<LaboratoryResponse>
     {
-        public string Id { get; set; }
+        public int LaboratoryId { get; set; }
         public string? Name { get; set; }
         public string? Location { get; set; }
         public string? ContactNumber { get; set; }
