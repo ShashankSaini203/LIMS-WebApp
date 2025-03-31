e574636d69d414ca1654141415063a87ac47a331
Shashank Saini <shashanksaini203@gmail.com>
2025-03-31 22:44:34 +0530

    Update Order command model updates

--- LIMS.Application/Commands/OrderDetails/UpdateOrderDetailsCommand.cs
+++ LIMS.Application/Commands/OrderDetails/UpdateOrderDetailsCommand.cs
@@ -6,11 +6,14 @@ namespace LIMS.Application.Commands.OrderDetails
     public class UpdateOrderDetailsCommand : IRequest<OrderDetailsResponse>
     {
         public int OrderId { get; set; }
-        public string? Name { get; set; }
-        public string? Manufacturer { get; set; }
-        public string? Model { get; set; }
-        public DateTime PurchaseDate { get; set; }
-        public DateTime ExpiryDate { get; set; }
+        public int? InstrumentId { get; set; }
+        public int? LaboratoryId { get; set; }
+        public int? Quantity { get; set; }
+        public string? Address { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string? OrderedBy { get; set; }
         public string? AdditionalNotes { get; set; }
+        public string? Status { get; set; }
     }
 }
