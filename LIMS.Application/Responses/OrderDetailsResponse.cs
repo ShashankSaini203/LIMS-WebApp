ee91c20302123a2ee004710214ea1fa2a7dc37f8
Shashank Saini <shashanksaini203@gmail.com>
2025-03-31 22:48:01 +0530

    OrderDetailsResponse updated

--- LIMS.Application/Responses/OrderDetailsResponse.cs
+++ LIMS.Application/Responses/OrderDetailsResponse.cs
@@ -3,16 +3,14 @@
     public class OrderDetailsResponse
     {
         public int OrderId { get; set; }
-
-        private const string DefaultStatus = "Pending";
+        public int InstrumentId { get; set; }
+        public int LaboratoryId { get; set; }
+        public int Quantity { get; set; }
+        public string Address { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime DeliveryDate { get; set; }
         public string OrderedBy { get; set; }
         public string AdditionalNotes { get; set; }
         public string Status => DefaultStatus;
-        public int Count { get; set; }
-        public int InstrumentId { get; set; }
-        public int LaboratoryId { get; set; }
-
     }
 }
