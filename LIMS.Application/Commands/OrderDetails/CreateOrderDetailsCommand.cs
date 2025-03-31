8e5a1d11018d909d8b115a1ab89483a02a803299
Shashank Saini <shashanksaini203@gmail.com>
2025-03-31 22:42:11 +0530

    Updated Create order details model

--- LIMS.Application/Commands/OrderDetails/CreateOrderDetailsCommand.cs
+++ LIMS.Application/Commands/OrderDetails/CreateOrderDetailsCommand.cs
@@ -1,13 +1,19 @@
 ﻿using MediatR;
 using LIMS.Application.Responses;
+using System.ComponentModel.DataAnnotations;
 
 namespace LIMS.Application.Commands.OrderDetails
 {
     public class CreateOrderDetailsCommand : IRequest<OrderDetailsResponse>
-    {        
-        public int LaboratoryId { get; set; }
+    {
         public int InstrumentId { get; set; }
-        public int Count { get; set; }
-
+        public int LaboratoryId { get; set; }
+        public int Quantity { get; set; }
+        public string Address { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string OrderedBy { get; set; }
+        public string AdditionalNotes { get; set; }
+        public string Status { get; set; }
     }
 }
