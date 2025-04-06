4febc5e2fb27410a9f47b45600cdc1d17c3ce556
Shashank Saini <shashanksaini203@gmail.com>
2025-04-06 23:07:01 +0530

    IOrderDetailsQueryRepository injected

--- LIMS.Application/Handlers/OrderDetails/OrderDetailsQueryHandler/GetAllOrderDetailsQueryHandler.cs
+++ LIMS.Application/Handlers/OrderDetails/OrderDetailsQueryHandler/GetAllOrderDetailsQueryHandler.cs
@@ -1,11 +1,18 @@
 ﻿using LIMS.Application.Queries.OrderDetails;
 using LIMS.Application.Responses;
+using LIMS.Domain.Interfaces.Repository.Query;
 using MediatR;
 
 namespace LIMS.Application.Handlers.OrderDetails.OrderDetailsQueryHandler
 {
     public class GetAllOrderDetailsQueryHandler : IRequestHandler<GetAllOrderDetailsQuery, IEnumerable<OrderDetailsResponse>>
     {
+        private readonly IOrderDetailsQueryRepository _orderDetailsQueryRepository;
+        public GetAllOrderDetailsQueryHandler(IOrderDetailsQueryRepository orderDetailsQueryRepository)
+        {
+            _orderDetailsQueryRepository = orderDetailsQueryRepository;
+        }
+
         Task<IEnumerable<OrderDetailsResponse>> IRequestHandler<GetAllOrderDetailsQuery, IEnumerable<OrderDetailsResponse>>.Handle(GetAllOrderDetailsQuery request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
