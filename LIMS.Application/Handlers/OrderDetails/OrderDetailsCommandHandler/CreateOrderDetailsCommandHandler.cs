fb3f959dac35c493125a29981d2bb604d55c5b7f
Shashank Saini <shashanksaini203@gmail.com>
2025-04-17 23:18:41 +0530

    IOrderDetailsCommandRepository injected

--- LIMS.Application/Handlers/OrderDetails/OrderDetailsCommandHandler/CreateOrderDetailsCommandHandler.cs
+++ LIMS.Application/Handlers/OrderDetails/OrderDetailsCommandHandler/CreateOrderDetailsCommandHandler.cs
@@ -1,11 +1,18 @@
 ﻿using LIMS.Application.Commands.OrderDetails;
 using LIMS.Application.Responses;
+using LIMS.Domain.Interfaces.Repository.Commands;
 using MediatR;
 
 namespace LIMS.Application.Handlers.OrderDetails.OrderDetailsCommandHandler
 {
     public class CreateOrderDetailsCommandHandler : IRequestHandler<CreateOrderDetailsCommand, OrderDetailsResponse>
     {
+        private readonly IOrderDetailsCommandRepository _orderDetailsCommandRepository;
+        public CreateOrderDetailsCommandHandler(IOrderDetailsCommandRepository orderDetailsCommandRepository)
+        {
+            _orderDetailsCommandRepository = orderDetailsCommandRepository;
+        }
+
         public Task<OrderDetailsResponse> Handle(CreateOrderDetailsCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
