4407de355c86882baf7fde1bf871a9b4c73d2f04
Shashank Saini <shashanksaini203@gmail.com>
2025-04-19 23:32:38 +0530

    IMediator injected into controller

--- LIMS.WebAPI/Controllers/OrderDetailsController.cs
+++ LIMS.WebAPI/Controllers/OrderDetailsController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LIMS.WebAPI.Controllers
 {
-    public class OrderDetailsController : Controller
+    public class OrderDetailsController : BaseController
     {
+        public OrderDetailsController(IMediator mediator) : base(mediator)
+        {
+
+        }
+
         public IActionResult Index()
         {
             return View();
