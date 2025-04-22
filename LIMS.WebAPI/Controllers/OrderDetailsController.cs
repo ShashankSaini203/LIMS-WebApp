52aac61051996af5340c99032abf2582f0c63ac7
Shashank Saini <shashanksaini203@gmail.com>
2025-04-22 23:37:13 +0530

    GetAllOrderDetails HttpGet Added

--- LIMS.WebAPI/Controllers/OrderDetailsController.cs
+++ LIMS.WebAPI/Controllers/OrderDetailsController.cs
@@ -1,10 +1,18 @@
-﻿using MediatR;
+﻿using LIMS.Application.Queries.OrderDetails;
+using LIMS.Domain.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LIMS.WebAPI.Controllers
 {
     public class OrderDetailsController : BaseController
     {
         public OrderDetailsController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpGet("GetAllOrderDetails")]
+        public async Task<IEnumerable<OrderDetails>> GetAllOrderDetails()
         {
         }
     }
