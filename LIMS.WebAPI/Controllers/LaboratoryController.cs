5c21de1cd9ac51226942b2af34cb81c2ee65e5f1
Shashank Saini <shashanksaini203@gmail.com>
2025-07-04 15:03:01 +0530

    Getting rid of FE for the time being

--- LIMS.WebAPI/Controllers/LaboratoryController.cs
+++ LIMS.WebAPI/Controllers/LaboratoryController.cs
@@ -29,56 +29,56 @@ namespace LIMS.WebApp.Controllers
 
         }
 
-        [HttpGet]
-        public async Task<IActionResult> UpdateLab(int id)
-        {
-            try
-            {
-                var labToEdit = await _mediator.Send(new GetLaboratoryByIdQuery(id));
-                return View(labToEdit);
-            }
-            catch (Exception)
-            {
-                return RedirectToAction(nameof(GetAllLabs));
-            }
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> UpdateLab(int id)
+        //{
+        //    try
+        //    {
+        //        var labToEdit = await _mediator.Send(new GetLaboratoryByIdQuery(id));
+        //        return View(labToEdit);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return RedirectToAction(nameof(GetAllLabs));
+        //    }
+        //}
 
-        [HttpPost]
-        public async Task<IActionResult> UpdateLab(UpdateLaboratoryCommand updatedLab)
-        {
-            try
-            {
-                if (!ModelState.IsValid)
-                {
-                    TempData["errorMessage"] = "Invalid data provided";
-                    return View();
-                }
+        //[HttpPost]
+        //public async Task<IActionResult> UpdateLab(UpdateLaboratoryCommand updatedLab)
+        //{
+        //    try
+        //    {
+        //        if (!ModelState.IsValid)
+        //        {
+        //            TempData["errorMessage"] = "Invalid data provided";
+        //            return View();
+        //        }
 
-                var updatedLabResult = await _mediator.Send(updatedLab);
-                TempData["successMessage"] = "Laboratory updated successfully!";
-                return RedirectToAction(nameof(GetAllLabs));
-            }
-            catch (Exception ex)
-            {
-                TempData["errorMessage"] = ex.ToString();
-                return View();
-            }
-        }
+        //        var updatedLabResult = await _mediator.Send(updatedLab);
+        //        TempData["successMessage"] = "Laboratory updated successfully!";
+        //        return RedirectToAction(nameof(GetAllLabs));
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TempData["errorMessage"] = ex.ToString();
+        //        return View();
+        //    }
+        //}
 
-        [HttpDelete]
-        public async Task<IActionResult> DeleteLab(int id)
-        {
-            try
-            {
-                await _mediator.Send(new DeleteLaboratoryCommand(id));
-                TempData["successMessage"] = "Laboratory deleted successfully!";
-                return RedirectToAction(nameof(GetAllLabs));
-            }
-            catch (Exception ex)
-            {
-                TempData["errorMessage"] = ex.ToString();
-                return View();
-            }
-        }
+        //[HttpDelete]
+        //public async Task<IActionResult> DeleteLab(int id)
+        //{
+        //    try
+        //    {
+        //        await _mediator.Send(new DeleteLaboratoryCommand(id));
+        //        TempData["successMessage"] = "Laboratory deleted successfully!";
+        //        return RedirectToAction(nameof(GetAllLabs));
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TempData["errorMessage"] = ex.ToString();
+        //        return View();
+        //    }
+        //}
     }
 }
