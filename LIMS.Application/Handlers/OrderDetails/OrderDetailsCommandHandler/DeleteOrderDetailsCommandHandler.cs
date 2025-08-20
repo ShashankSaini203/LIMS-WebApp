02783d74c7230727b0fc3a5dddc8b9ad751b9a6d
Shashank Saini <shashanksaini203@gmail.com>
2025-08-20 22:19:30 +0530

    Added private fields

--- LIMS.Application/Handlers/OrderDetails/OrderDetailsCommandHandler/DeleteOrderDetailsCommandHandler.cs
+++ LIMS.Application/Handlers/OrderDetails/OrderDetailsCommandHandler/DeleteOrderDetailsCommandHandler.cs
@@ -1,10 +1,15 @@
 ﻿using LIMS.Application.Commands.OrderDetails;
+using LIMS.Domain.Interfaces.Repository.Commands;
+using LIMS.Domain.Interfaces.Repository.Query;
 using MediatR;
 
 namespace LIMS.Application.Handlers.OrderDetails.OrderDetailsCommandHandler
 {
     public class DeleteOrderDetailsCommandHandler : IRequestHandler<DeleteOrderDetailsCommand, Unit>
     {
+        private IOrderDetailsCommandRepository _orderDetailsCommandRepository;
+        private IOrderDetailsQueryRepository _orderDetailsQueryRepository;
+
         Task<Unit> IRequestHandler<DeleteOrderDetailsCommand, Unit>.Handle(DeleteOrderDetailsCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
