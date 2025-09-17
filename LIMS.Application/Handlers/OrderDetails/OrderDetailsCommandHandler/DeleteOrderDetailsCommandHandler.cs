8562bc53c2f975b55d1d0ab1cc5c17865465fe74
Shashank Saini <shashanksaini203@gmail.com>
2025-09-17 21:55:50 +0530

    Implemented DeleteOrderDetailsCommandHandler

--- LIMS.Application/Handlers/OrderDetails/OrderDetailsCommandHandler/DeleteOrderDetailsCommandHandler.cs
+++ LIMS.Application/Handlers/OrderDetails/OrderDetailsCommandHandler/DeleteOrderDetailsCommandHandler.cs
@@ -16,9 +16,24 @@ namespace LIMS.Application.Handlers.OrderDetails.OrderDetailsCommandHandler
             _orderDetailsQueryRepository = orderDetailsQueryRepository;
         }
 
-        Task<Unit> IRequestHandler<DeleteOrderDetailsCommand, Unit>.Handle(DeleteOrderDetailsCommand request, CancellationToken cancellationToken)
+        async Task<Unit> IRequestHandler<DeleteOrderDetailsCommand, Unit>.Handle(DeleteOrderDetailsCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var entityToDelete = await _orderDetailsQueryRepository.GetAsyncById(request.OrderId, Domain.Common.DataTables.OrderDetailTable, Domain.Common.DataColumns.OrderId);
+
+            if (entityToDelete == null)
+            {
+                throw new Exception($"No OrderDetails with Id {request.OrderId} found");
+            }
+
+            var orderEntity = LIMS.Application.Mappers.AutoMapperConfiguration.Mapper.Map<Domain.Models.OrderDetails>(entityToDelete);
+            if (orderEntity == null)
+            {
+                throw new ApplicationException("Unable to map due to an issue with mapper.");
+            }
+
+            await _orderDetailsCommandRepository.DeleteAsync(orderEntity);
+
+            return Unit.Value;
         }
     }
 }
