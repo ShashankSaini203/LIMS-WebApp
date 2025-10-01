9e0ede1e1ef4128c26b897edb3803045088232fc
Shashank Saini <shashanksaini203@gmail.com>
2025-10-01 22:12:30 +0530

    Order Details fixes

---
+++ LIMS.Application/Queries/OrderDetails/GetOrderDetailsByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using LIMS.Application.Responses;
+using MediatR;
+
+namespace LIMS.Application.Queries.OrderDetails
+{
+    public class GetOrderDetailsByIdQuery : IRequest<OrderDetailsResponse>
+    {
+        public int OrderId { get; set; }
+
+        public GetOrderDetailsByIdQuery(int orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
