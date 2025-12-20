8dbe350fa8ac72d180afe2fb32c10f997b2f2a17
Shashank Saini <shashanksaini203@gmail.com>
2025-12-20 22:37:05 +0530

    CreateCalibrationRecordCommandHandler Added

---
+++ LIMS.Application/Handlers/CalibrationRecord/CalibrationRecordCommandHandler/CreateCalibrationRecordCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using LIMS.Application.Commands.CalibrationRecord;
+using LIMS.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMS.Application.Handlers.CalibrationRecord.CalibrationRecordCommandHandler
+{
+    public class CreateCalibrationRecordCommandHandler
+    {
+
+    }
+}
