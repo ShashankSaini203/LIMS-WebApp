adeeb50f02beec993dc3b763502480fd22d05d53
Shashank Saini <shashanksaini203@gmail.com>
2026-01-14 22:51:22 +0530

    Repository command for DB added

--- LIMS.Infrastructure/Repository/Commands/CalibrationRecordCommandRepository.cs
+++ LIMS.Infrastructure/Repository/Commands/CalibrationRecordCommandRepository.cs
@@ -1,4 +1,5 @@
-﻿using LIMS.Domain.Interfaces.Repository.Commands;
+﻿using LIMS.Application.Responses;
+using LIMS.Domain.Interfaces.Repository.Commands;
 using LIMS.Domain.Models;
 using LIMS.Infrastructure.Database.DBcontext;
 using LIMS.Infrastructure.Repository.Commands.BaseCommand;
@@ -7,10 +8,35 @@ namespace LIMS.Infrastructure.Repository.Commands
 {
     public class CalibrationRecordCommandRepository : BaseCommandRepository<CalibrationRecord>, ICalibrationRecordCommandRepository
     {
-        private DataContext dataContext;
+        private DataContext _dataContext;
 
         public CalibrationRecordCommandRepository(DataContext dataContext) : base(dataContext)
         {
+            _dataContext = dataContext;
+        }
+
+        public override async Task<CalibrationRecord> CreateAsync(CalibrationRecord entity)
+        {
+            if (entity is null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            var isInstrumentAvailabe = await _dataContext.Set<Instrument>().FindAsync(entity.InstrumentId);
+            if (isInstrumentAvailabe is null)
+            {
+                throw new KeyNotFoundException($"No Instrument found with the provided id {entity.InstrumentId}");
+            }
+
+            var isTechnicianAvailable = await _dataContext.Set<Technician>().FindAsync(entity.TechnicianId);
+            if (isTechnicianAvailable is null)
+            {
+                throw new KeyNotFoundException($"No Technician available with the provided id {entity.TechnicianId}");
+            }
+
+            var addedCalibrationRecord= _dataContext.CalibrationRecords.Add(entity);
+            await _dataContext.SaveChangesAsync();
+            return addedCalibrationRecord.Entity;
+
         }
 
     }
